aba86de94153ad6e30e809b73479a67d2a09a606
Barbara Law <barbara.rm.law@gmail.com>
2024-02-16 15:28:09 -0800

    wip

--- src/routes/coloring.jsx
+++ src/routes/coloring.jsx
@@ -1,46 +1,128 @@
-import coloring_1 from "../assets/coloring/coloring_1.png"
-import coloring_2 from "../assets/coloring/coloring_2.png"
-import coloring_3 from "../assets/coloring/coloring_3.png"
-import coloring_4 from "../assets/coloring/coloring_4.png"
-import coloring_5 from "../assets/coloring/coloring_5.png"
-import coloring_6 from "../assets/coloring/coloring_6.png"
-import coloring_7 from "../assets/coloring/coloring_7.png"
-import coloring_8 from "../assets/coloring/coloring_8.png"
-import coloring_9 from "../assets/coloring/coloring_9.png"
-import coloring_10 from "../assets/coloring/coloring_10.png"
-import coloring_11 from "../assets/coloring/coloring_11.png"
-import coloring_12 from "../assets/coloring/coloring_12.png"
-import coloring_13 from "../assets/coloring/coloring_13.png"
-import coloring_14 from "../assets/coloring/coloring_14.png"
+import coloring_1 from "../assets/coloring/coloring_1.png";
+import coloring_2 from "../assets/coloring/coloring_2.png";
+import coloring_3 from "../assets/coloring/coloring_3.png";
+import coloring_4 from "../assets/coloring/coloring_4.png";
+import coloring_5 from "../assets/coloring/coloring_5.png";
+import coloring_6 from "../assets/coloring/coloring_6.png";
+import coloring_7 from "../assets/coloring/coloring_7.png";
+import coloring_8 from "../assets/coloring/coloring_8.png";
+import coloring_9 from "../assets/coloring/coloring_9.png";
+import coloring_10 from "../assets/coloring/coloring_10.png";
+import coloring_11 from "../assets/coloring/coloring_11.png";
+import coloring_12 from "../assets/coloring/coloring_12.png";
+import coloring_13 from "../assets/coloring/coloring_13.png";
+import coloring_14 from "../assets/coloring/coloring_14.png";
+import coloring_15 from "../assets/coloring/coloring_15.png";
+import coloring_16 from "../assets/coloring/coloring_16.png";
+import coloring_17 from "../assets/coloring/coloring_17.png";
+import coloring_18 from "../assets/coloring/coloring_18.png";
+import coloring_19 from "../assets/coloring/coloring_19.png";
+import coloring_20 from "../assets/coloring/coloring_20.png";
+import coloring_21 from "../assets/coloring/coloring_21.png";
+import coloring_22 from "../assets/coloring/coloring_22.png";
+import coloring_23 from "../assets/coloring/coloring_23.png";
+import coloring_24 from "../assets/coloring/coloring_24.png";
+import coloring_25 from "../assets/coloring/coloring_25.png";
+import coloring_26 from "../assets/coloring/coloring_26.png";
+import coloring_27 from "../assets/coloring/coloring_27.png";
+import coloring_28 from "../assets/coloring/coloring_28.png";
+import coloring_29 from "../assets/coloring/coloring_29.png";
+import coloring_30 from "../assets/coloring/coloring_30.png";
+import coloring_31 from "../assets/coloring/coloring_31.png";
+import coloring_32 from "../assets/coloring/coloring_32.png";
+import coloring_33 from "../assets/coloring/coloring_33.png";
+import coloring_34 from "../assets/coloring/coloring_34.png";
+import coloring_35 from "../assets/coloring/coloring_35.png";
+import coloring_36 from "../assets/coloring/coloring_36.png";
+import coloring_37 from "../assets/coloring/coloring_37.png";
+import coloring_38 from "../assets/coloring/coloring_38.png";
+import coloring_39 from "../assets/coloring/coloring_39.png";
+import coloring_40 from "../assets/coloring/coloring_40.png";
+import coloring_41 from "../assets/coloring/coloring_41.png";
+import coloring_42 from "../assets/coloring/coloring_42.png";
+import coloring_43 from "../assets/coloring/coloring_43.png";
+import coloring_44 from "../assets/coloring/coloring_44.png";
+import coloring_45 from "../assets/coloring/coloring_45.png";
+import coloring_46 from "../assets/coloring/coloring_46.png";
+import coloring_47 from "../assets/coloring/coloring_47.png";
+import coloring_48 from "../assets/coloring/coloring_48.png";
+import coloring_49 from "../assets/coloring/coloring_49.png";
+import coloring_50 from "../assets/coloring/coloring_50.png";
+import coloring_51 from "../assets/coloring/coloring_51.png";
+import coloring_52 from "../assets/coloring/coloring_52.png";
+import coloring_53 from "../assets/coloring/coloring_53.png";
+import coloring_54 from "../assets/coloring/coloring_54.png";
 
 export const Coloring = () => {
-    return (
-        <div>
-            <h1>Coloring Pages</h1>
-            <p>To print a coloring page, right click on the image you want and open it in a new tab. The picture will be full size and you can then print it and enjoy!</p>
-            <p><b>Fun Fact:</b> All of these coloring pages were generated for free by Google Gemini. It was a lot of fun to figure out how to get the AI to create them!</p>
-            <div id="coloringPages">
-
-                <img src={coloring_1} />
-
-                <img src={coloring_2}  />
-                <img src={coloring_3} />
-                <img src={coloring_4}  />
-
-                <img src={coloring_5}  />
-                <img src={coloring_6} />
-                <img src={coloring_7}  />
-
-                <img src={coloring_8}/>
-                <img src={coloring_9}  />
-                <img src={coloring_10}  />
-                <img src={coloring_11} />
-                <img src={coloring_12}  />
-
-                <img src={coloring_13}/>
-                <img src={coloring_14}  />
-            </div>
-
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Coloring Pages</h1>
+      <p>
+        To print a coloring page, right click on the image you want and open it
+        in a new tab. The picture will be full size and you can then print it
+        and enjoy!
+      </p>
+      <p>
+        <b>Fun Fact:</b> All of these coloring pages were generated for free by
+        Google Gemini. It was a lot of fun to figure out how to get the AI to
+        create them!
+      </p>
+      <div id="coloringPages">
+        <img src={coloring_1} />
+        <img src={coloring_2} />
+        <img src={coloring_3} />
+        <img src={coloring_4} />
+        <img src={coloring_5} />
+        <img src={coloring_6} />
+        <img src={coloring_7} />
+        <img src={coloring_8} />
+        <img src={coloring_9} />
+        <img src={coloring_10} />
+        <img src={coloring_11} />
+        <img src={coloring_12} />
+        <img src={coloring_13} />
+        <img src={coloring_14} />
+        <img src={coloring_15} />
+        <img src={coloring_16} />
+        <img src={coloring_17} />
+        <img src={coloring_18} />
+        <img src={coloring_19} />
+        <img src={coloring_20} />
+        <img src={coloring_21} />
+        <img src={coloring_22} />
+        <img src={coloring_23} />
+        <img src={coloring_24} />
+        <img src={coloring_25} />
+        <img src={coloring_26} />
+        <img src={coloring_27} />
+        <img src={coloring_28} />
+        <img src={coloring_29} />
+        <img src={coloring_30} />
+        <img src={coloring_31} />
+        <img src={coloring_32} />
+        <img src={coloring_33} />
+        <img src={coloring_34} />
+        <img src={coloring_35} />
+        <img src={coloring_36} />
+        <img src={coloring_37} />
+        <img src={coloring_38} />
+        <img src={coloring_39} />
+        <img src={coloring_40} />
+        <img src={coloring_41} />
+        <img src={coloring_42} />
+        <img src={coloring_43} />
+        <img src={coloring_44} />
+        <img src={coloring_45} />
+        <img src={coloring_46} />
+        <img src={coloring_47} />
+        <img src={coloring_48} />
+        <img src={coloring_49} />
+        <img src={coloring_50} />
+        <img src={coloring_51} />
+        <img src={coloring_52} />
+        <img src={coloring_53} />
+        <img src={coloring_54} />
+      </div>
+    </div>
+  );
+};
